ff80cc929c9faf7cacef56402394761bacac5a38
DingTongLin <1250753511@qq.com>
2020-07-06 20:33:53 +0800

    第二次提交 添加首页模板

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
-
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'hello world'
+    return render_template('home.html')
 
 
 if __name__ == '__main__'
+    app.debug = True   
     app.run()
